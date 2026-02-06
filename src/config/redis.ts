c75f89c47c44b77f1f25a88d7868d1990abbad96
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-02-06 21:16:20 +0530

    Update redis.ts

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,28 @@
+// import Redis from "ioredis";
+
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: Number(process.env.REDIS_PORT),
+// });
+
+// redis.on("connect", () => {
+//   console.log("✅ Redis connected");
+// });
+
+// redis.on("error", (err) => {
+//   console.error("❌ Redis error", err);
+// });
+
+// export default redis;
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-});
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  throw new Error("REDIS_URL is not defined");
+}
+
+const redis = new Redis(redisUrl);
 
 redis.on("connect", () => {
   console.log("✅ Redis connected");
